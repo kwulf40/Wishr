141a999f95578a469287180b3d6f304822fae25b
Wo1fbite <kwulf40@gmail.com>
2021-03-25 02:52:54 -0700

    First pass URL Sanitation
    Early prototype to clean url references from the product text.
    Additionally, created a URL whitelist for the extension to only support Target.com, Walmart.com, and Amazon.com

--- content.js
+++ content.js
@@ -1,9 +1,23 @@
 chrome.runtime.onMessage.addListener(function (request) {
+    var urlList = ["Amazon.com", "Walmart.com", "Target.com", "Walmart", ".com"];
     var htmlText = document.getElementsByTagName('title')[0].text;
 
 	if(htmlText)
 	{
-		alert(htmlText)
-        console.log(htmlText)
+        htmlText = cleanURL(urlList, htmlText);
+        console.log("test")
+        if (htmlText){
+		    alert(htmlText)
+            console.log(htmlText)
+            console.log("test2")
+        }
 	}
+
+    function cleanURL(urlListIn, inText){
+        var cleanString = inText;
+        for (var i = 0; i < urlListIn.length; i++){
+            cleanString = cleanString.replace(urlListIn[i], "");
+        }
+        return cleanString;
+    }
 })
