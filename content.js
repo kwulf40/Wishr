81151cf7b3ee538140f2acc6d0ba989d75fb42cb
kwulf40 <kwulf40@gmail.com>
2021-03-25 18:19:31 -0700

    Split title string by ":" character to filter product name from excess information

--- content.js
+++ content.js
@@ -1,23 +1,29 @@
 chrome.runtime.onMessage.addListener(function (request) {
     var urlList = ["Amazon.com", "Walmart.com", "Target.com", "Walmart", ".com"];
     var htmlText = document.getElementsByTagName('title')[0].text;
+    var cleanTextArray = []
 
 	if(htmlText)
 	{
-        htmlText = cleanURL(urlList, htmlText);
+        cleanTextArray = cleanURL(urlList, htmlText);
         console.log("test")
-        if (htmlText){
-		    alert(htmlText)
-            console.log(htmlText)
-            console.log("test2")
+        if (cleanTextArray){
+            for(var i = 0; i < cleanTextArray.length; i++){
+		    alert(cleanTextArray[i]);
+            console.log(cleanTextArray[i]);
+            console.log("test" + i);
+            }
         }
 	}
 
     function cleanURL(urlListIn, inText){
-        var cleanString = inText;
-        for (var i = 0; i < urlListIn.length; i++){
-            cleanString = cleanString.replace(urlListIn[i], "");
+        var cleanString = inText.split(":");
+        
+    for (var i = 0; i < cleanString.length; i++){
+        for (var j = 0; j < urlListIn.length; j++){
+            cleanString[i] = cleanString[i].replace(urlListIn[j], "");
         }
+    }
         return cleanString;
     }
 })
