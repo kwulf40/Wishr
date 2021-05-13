69f6c0d01e8271ef63008671407d334ae6cffd5c
kwulf40 <kwulf40@gmail.com>
2021-05-13 02:32:05 -0700

    Wishlist items now display in a table

--- wishlist.js
+++ wishlist.js
@@ -18,14 +18,18 @@ window.onload = function() {
 
                     var i;
                     var listItems = xmlDoc.getElementsByTagName('listitem');
-                    var html = '';
+                    var html = '<table class=\"WishlistTable\"><tbody>';
+                    var headers = '<tr><th style="padding-left: 100px; padding-right: 100px;">Item</th><th style="padding-left: 15px; text-align:center;">Retailer</th><th style="padding-left: 40px; text-align:right;">Price</th></tr>'
+                    html += headers;
                     for (i = 0; i < listItems.length; i++) { 
                         // build the HTML for the image and name text
                         console.log(listItems[i].getElementsByTagName('itemName')[0].childNodes[0].nodeValue)
-                        html += `<div class="item"><a href = "${listItems[i].getElementsByTagName('itemURL')[0].childNodes[0].nodeValue}" target="_blank">${listItems[i].getElementsByTagName('itemName')[0].childNodes[0].nodeValue}</a></div>`;
-                        html += "<div class=\"retailer\">" + "<p>" + listItems[i].getElementsByTagName('mainRetailer')[0].childNodes[0].nodeValue + "</p>" + "</div>";
-                        html += "<div class=\"price\">" + "<p>" + listItems[i].getElementsByTagName('mainPrice')[0].childNodes[0].nodeValue + "</p>" + "</div>";
+                        html += `<tr><td class="item"><a href = "${listItems[i].getElementsByTagName('itemURL')[0].childNodes[0].nodeValue}" target="_blank">${listItems[i].getElementsByTagName('itemName')[0].childNodes[0].nodeValue}</a></td>`;
+                        html += "<td class=\"retailer\">"  + listItems[i].getElementsByTagName('mainRetailer')[0].childNodes[0].nodeValue + "</td>";
+                        html += "<td class=\"price\">" + listItems[i].getElementsByTagName('mainPrice')[0].childNodes[0].nodeValue + "</td></tr>";
+                        //html += "<div class=\"button\"><span class=\"close\">&times;</span></div>"
                     }
+                    html += "</tbody></table>"
                     document.getElementById("items").innerHTML = html;
                     console.log(xmlDoc)
                 }
